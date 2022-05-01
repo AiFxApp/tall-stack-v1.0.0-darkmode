696dc4b1fd148e3cc17857780a998917c6bdce6f
AiFx4Intel <89227642+AiFxApp@users.noreply.github.com>
2022-05-01 15:51:57 +0200

    added hover cards (nice effect) underline to match

--- resources/views/components/hover-card.blade.php
+++ resources/views/components/hover-card.blade.php
@@ -3,28 +3,28 @@
     <div class="bg-hover w-52 h-72 m-8 static rounded-lg ">
         <div class="bg-std dark:bg-dark2 w-52 h-72 -m-2 hover:m-0 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
             <h1 class="m-4 text-2xl font-bold text-dark dark:text-std ">Wat is Lorem Ipsum?</h1>
-            <hr class="m-4 rounded-2xl border-t-2 text-dark dark:text-hover">
+            <hr class="m-4 rounded-2xl border-t-2 text-hover dark:text-hover">
             <p class="m-4 text-sm text-dark dark:text-std">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak</p>
         </div>
     </div>
     <div class="bg-green w-52 h-72 m-8 static rounded-lg ">
         <div class="bg-std dark:bg-dark2 w-52 h-72 hover:-m-2 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
             <h1 class="m-4 text-2xl font-bold text-dark dark:text-std">Wat is Lorem Ipsum?</h1>
-            <hr class="m-4 rounded-2xl border-t-2 text-dark dark:text-green">
+            <hr class="m-4 rounded-2xl border-t-2 text-green dark:text-green">
             <p class="m-4 text-sm text-dark dark:text-std">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak</p>
         </div>
     </div>
     <div class="bg-red w-52 h-72 m-8 static rounded-lg ">
         <div class="bg-std dark:bg-dark2 w-52 h-72 hover:m-2 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
             <h1 class="m-4 text-2xl font-bold text-dark dark:text-std">Wat is Lorem Ipsum?</h1>
-            <hr class="m-4 rounded-2xl border-t-2 text-dark dark:text-red">
+            <hr class="m-4 rounded-2xl border-t-2 text-red dark:text-red">
             <p class="m-4 text-sm text-dark dark:text-std">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak</p>
         </div>
     </div>
     <div class="bg-violet w-52 h-72 m-8 static rounded-lg ">
         <div class="bg-std dark:bg-dark2 w-52 h-72 m-2 hover:m-0 absolute rounded-lg shadow-lg hover:shadow-2xl transition-all duration-150 ease-out hover:ease-in ">
             <h1 class="m-4 text-2xl font-bold text-dark dark:text-std">Wat is Lorem Ipsum?</h1>
-            <hr class="m-4 rounded-2xl border-t-2 text-dark dark:text-violet">
+            <hr class="m-4 rounded-2xl border-t-2 text-violet dark:text-violet">
             <p class="m-4 text-sm text-dark dark:text-std">Lorem Ipsum is slechts een proeftekst uit het drukkerij- en zetterijwezen. Lorem Ipsum is de standaard proeftekst in deze bedrijfstak sinds de 16e eeuw, toen een onbekende drukker een zethaak</p>
         </div>
     </div>
